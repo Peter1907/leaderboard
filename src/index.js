cc93d9468fd3f0603922d95358a185c64fc80303
Peter1907 <peter.nady.1408@gmail.com>
2022-08-24 02:05:00 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const list = [100, 20, 50, 78, 125, 77, 42];
 const board = document.querySelector('.board');
-list.forEach(item => {
+list.forEach((item) => {
   const li = document.createElement('li');
   li.innerText = `Name: ${item}`;
   board.appendChild(li);
