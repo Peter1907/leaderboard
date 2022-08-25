b7f5d25088433faf1a4db136c62b6b4e206adc7b
Peter1907 <peter.nady.1408@gmail.com>
2022-08-25 11:57:42 +0200

    Include API to add & refresh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import './style.css';
 
-const list = [100, 20, 50, 78, 125, 77, 42];
+let list = [];
+async function getList() {
+  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rdFXKRDKu4NbpLCUv45S/scores');
+  const data = await res.json();
+  list = [];
+  data.result.forEach(element => {
+    list.push(element);    
+  });
+};
+
 const board = document.querySelector('.board');
-list.forEach((item) => {
-  const li = document.createElement('li');
-  li.innerText = `Name: ${item}`;
-  board.appendChild(li);
+const refreshBtn = document.getElementById('refresh');
+const display = async () => {
+  await getList();
+  board.innerHTML = '';
+  list.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = `${item.user}: ${item.score}`;
+    board.appendChild(li);
+  });
+};
+display();
+
+refreshBtn.addEventListener('click', display);
+
+const AddBtn = document.getElementById('add');
+
+AddBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name');
+  const score = document.getElementById('score');
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rdFXKRDKu4NbpLCUv45S/scores', {
+  method: 'POST',
+  body: JSON.stringify({
+    user: name.value,
+    score: score.value,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  name.value = '';
+  score.value = '';
 });
